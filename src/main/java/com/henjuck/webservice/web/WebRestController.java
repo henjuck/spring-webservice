02f9d80bc8e6d0f38a5c702a5bfb37838c1fb436
henjuck <henjcuk@gmail.com>
2020-01-10 16:09:17 +0900

    첫 Hello World

---
+++ src/main/java/com/henjuck/webservice/web/WebRestController.java
@@ -0,0 +1,19 @@
+package com.henjuck.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * WebRestController
+ */
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!!";
+
+    }
+
+}
